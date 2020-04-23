45a6f3ca754fadaf81666a9838c9ac273bed6cee
Rahul <rahulbhargava010@gmail.com>
2020-04-23 14:46:04 +0530

    added build folder

---
+++ client/build/precache-manifest.261628dd0b1bb29541e683652db00561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484c2c92242a15931a80dc82cd61ad4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b83de977d010057319",
+    "url": "/static/css/main.28d447f5.chunk.css"
+  },
+  {
+    "revision": "06310a3e68f5f4fba3bb",
+    "url": "/static/js/2.acb5fe66.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.acb5fe66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b83de977d010057319",
+    "url": "/static/js/main.54ffd83c.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
