e293296a3114e38a7d44de38ca6973c3189c4967
Rahul <rahulbhargava010@gmail.com>
2020-05-07 11:59:54 +0530

    new build with expense with user

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26b71e9a72a6c49bb7089d370ebde3fe.js"
+  "/precache-manifest.3b4c1e9f45d677aa9358d83dc8a5acff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
