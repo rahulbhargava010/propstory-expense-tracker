d4bc4c699498dec0afb33f42ce7f873a40c7d9bb
root <root@ip-172-31-33-155.ap-south-1.compute.internal>
2020-06-05 10:00:52 +0000

    server problem start from here

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39d2ba498e7585f3fd78335b283238f5.js"
+  "/precache-manifest.cfcd16b016fc505d5dc17fc02e25e9d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
