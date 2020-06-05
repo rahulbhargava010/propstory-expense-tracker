a845226d76494392469aa5285f36a691aceeba05
moh-hit <moh.hit1012@gmail.com>
2020-06-05 15:51:31 +0530

    AFTER PROBLEM LATEST  WITH BUILD

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.afe7a6732753581977235d497850e194.js"
+  "/precache-manifest.ce980ce296fe316e360920562b291225.js"
 );
 
 self.addEventListener('message', (event) => {
--- public/precache-manifest.ce980ce296fe316e360920562b291225.js
+++ public/precache-manifest.ce980ce296fe316e360920562b291225.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "615cc3eeb1eafe94e849a11f0bdf148d",
+    "revision": "4c817db02ef15f2956415cddb5c6a287",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.a1840750.chunk.css"
   },
   {
-    "revision": "9fdb55b9046391d7fe95",
+    "revision": "ddccbce5743e0332db34",
     "url": "/static/css/main.ced9ab1c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1dd9f48b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9fdb55b9046391d7fe95",
-    "url": "/static/js/main.215e1161.chunk.js"
+    "revision": "ddccbce5743e0332db34",
+    "url": "/static/js/main.0d98d965.chunk.js"
   },
   {
     "revision": "0e35831b56fb2c044e12",
